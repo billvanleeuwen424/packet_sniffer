c288b40c95df3b7ede2ecd1ea0a7fc11657a5eac
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2026-02-15 21:43:28 -0500

    cli handling, takes interface, produces errors, tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod args;
+mod error;
+
+use args::Args;
+use clap::Parser;
+use error::AppError;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), AppError> {
+    let args = Args::parse();
+
+    match &args.interface {
+        Some(name) => {
+            println!("interface: {}", name);
+
+            Ok(())
+        }
+        None => {
+            println!("interface: None given");
+
+            Ok(())
+        }
+    }
 }
